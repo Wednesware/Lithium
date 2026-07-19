3f3f2b57e6aa7a5a523a146cc4538e8b13b75565
Ves <146763980+vesperyn@users.noreply.github.com>
2026-07-19 13:59:21 +0200

    Delete perkeo directory

--- perkeo/__main__.py
+++
@@ -1,6 +0,0 @@
-from ww.he import Project
-
-
-perkeo: Project = Project(__file__, ".")
-
-perkeo.script.runpk()
--- perkeo/resources/ReturnSignal.py
+++
@@ -1,5 +0,0 @@
-class ReturnSignal(Exception):
-    def __init__(self, value: any = None, layers: int = 1):
-        self.value = value
-        self.layers = layers
-        super().__init__(value, layers)
